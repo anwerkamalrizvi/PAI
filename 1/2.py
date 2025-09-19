508958000b9f8cee3a96213099a1bb4a675e477b
anwerkamalrizvi <sanwerkamal@gmail.com>
2025-09-19 12:04:03 +0500

    Add files via upload

--- 1/2.py
+++
@@ -1,5 +0,0 @@
-s = "Artificial Intelligence"
-print(s[12:16])
-print(s[::-1])
-print(s[::2])
-print(s[-21:-18])
--- 1/3.py
+++
@@ -1,8 +0,0 @@
-num = input("Enter a 4-digit number: ")
-if len(num) == 4:
-    swapped = num[3] + num[2] + num[1] + num[0]
-    encrypted = ""
-    for digit in swapped:
-        new_digit = (int(digit) + 7) % 10
-        encrypted += str(new_digit)
-    print("Encrypted number IS:", encrypted)
